604f42667895325f5c4f702e92847fdfff5be1fb
theonefoster <6105690+theonefoster@users.noreply.github.com>
2022-07-04 16:16:13 +0100

    Create InfiniteThread.py
    Added InfiniteThreads, which automatically restart if there's any exception

---
+++ InfiniteThread.py
@@ -0,0 +1,30 @@
+import threading
+from time import time, sleep
+
+class InfiniteThread():
+	def __init__(self, target=None, name=None, log_func=print):
+		self.target_func = target
+		self.thread_name = name
+		self.log_func = log_func
+		self.last_error = 0
+		self.dropoff = 2
+		self.log_func(f"InfiniteThread created with name {self.thread_name} and target {self.target_func}")
+
+	def catch_exceptions(self, args):
+		if self.last_error < time() - (self.dropoff*3): # reset dropoff after a while
+			self.dropoff = 1
+
+		self.last_error = time()
+		self.dropoff *= 2
+		timeout = self.dropoff
+		self.log_func(f"Exception in {self.thread_name}: {args[0].__name__} - {str(args[1])} - sleeping for {round(timeout, 1)}s..")
+		sleep(timeout)
+
+		t = threading.Thread(target=self.target_func, name=self.thread_name)
+		threading.excepthook=self.catch_exceptions
+		t.start()
+
+	def start(self):
+		t = threading.Thread(target=self.target_func, name=self.thread_name)
+		threading.excepthook=self.catch_exceptions
+		t.start()
